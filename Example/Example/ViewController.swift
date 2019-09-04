7d1cff522a63271d5949f98c742d610c20ff3fa9
iMoe <iMoeNya@outlook.com>
2019-09-04 22:48:36 +0800

    Update Demo

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import EnumeratedTable
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
 }
 
+
+// MARK: Enumerated
+extension ViewController {
+    enum Section: Int, SectionEnumeration {
+        case section1
+        case section2
+        
+        var RowsInSection: RowEnumerated.Type {
+            return Row.self
+        }
+    }
+    
+    enum Row: Int, RowEnumeration {
+        case row1
+        
+        var text: String? {
+            return "row1"
+        }
+    }
+}
+
+// MARK: Table View Data Source
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return Section.cases.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = Section(index: section)!
+        return section.RowsInSection.cases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = Row(index: indexPath.row)!
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = row.text
+        return cell
+    }
+}
+
+// MARK: Table View Delegate
+extension ViewController: UITableViewDelegate {
+    
+}
