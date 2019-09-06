687cb55106b4ea034249e96f4f620d1781f35b07
iMoe <iMoeNya@outlook.com>
2019-09-07 00:34:24 +0800

    Row enumeration provides row height

--- Source/RowEnumerated.swift
+++ Source/RowEnumerated.swift
@@ -10,14 +10,22 @@ import Foundation
 
 /// Describe a row of the table.
 public protocol RowEnumerated: Enumerated {
+    
     /// The reuse identifier
     var reuseIdentifier: String? { get }
+    
     /// Text in `textLabel` of a row or title of a section.
     var text: String? { get }
+    
     /// Text in `detailLabel` of a row.
     var detail: String? { get }
+    
+    /// The height of the row, or nil to auto layout the cell.
+    var height: CGFloat? { get }
+    
     /// Describe how the cell should be configured.
     func configureEnumerableCell(_ cell: Enumerable)
+    
     /// Describe how the table should behave when cell is selected. Defaults to doing
     /// nothing at all.
     func handleSelection(by viewController: UIViewController)
@@ -28,6 +36,8 @@ public extension RowEnumerated {
     var text: String? { return nil }
     
     var detail: String? { return nil }
+    
+    var height: CGFloat? { return nil }
     
     func configureEnumerableCell(_ cell: Enumerable) {
         cell.configure(using: self)
